61b80326bbd6c5750c2355292118f6f9592a68d2
victoriawordsworth <n9817204@qut.edu.au>
2024-03-19 19:18:51 +1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Victoria Wordsworth`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
